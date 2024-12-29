a6237e8fe92c2e3469f3e0d8c60273caa474504b
chubao-uk <chubao.uk@gmail.com>
2024-12-29 23:23:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
   domainSource: "https://2fa15b2a-5384-4834-9f6b-b074287ab397.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/product/[^/]+",
+          pattern: "/store/product/[^/]+",
           metaDataEndpoint: "https://xp7p-n0ok-fjrr.e2.xano.io/api:WC7yTG1w/store_product_metadata/{store_product_id}"
       },
       // Add more patterns and their metadata endpoints as needed
